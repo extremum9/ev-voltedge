658468230eca58f62a39093201376d05d5355e71
extremum9 <denprogman99@gmail.com>
2026-06-09 23:12:37 +0300

    feat: add interior-gallery section styles

--- src/main.js
+++ src/main.js
@@ -1,3 +1,28 @@
+import Swiper from 'swiper';
+import { A11y, Pagination } from 'swiper/modules';
+
 import initTopbar from './modules/topbar';
 
 initTopbar();
+
+new Swiper('.js-interior-gallery-slider', {
+  modules: [Pagination, A11y],
+  pagination: {
+    el: '.interior-gallery-slider-pagination',
+    clickable: true
+  },
+  slidesPerView: 3,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 14
+    },
+    480: {
+      slidesPerView: 2
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    }
+  }
+});
